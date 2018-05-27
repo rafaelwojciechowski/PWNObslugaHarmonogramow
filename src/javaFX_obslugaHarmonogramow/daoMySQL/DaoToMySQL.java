eeaefa257556d94aa7240b1132fbd6b6077a096b
ilowiak <ilowiak@gmail.com>
2018-05-27 13:12:21 +0200

    dodanie kodu do DaoToMySQL

--- src/javaFX_obslugaHarmonogramow/daoMySQL/DaoToMySQL.java
+++ src/javaFX_obslugaHarmonogramow/daoMySQL/DaoToMySQL.java
@@ -1,4 +1,37 @@
 package javaFX_obslugaHarmonogramow.daoMySQL;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DaoToMySQL {
+    final static String draiverMySQl = "com.mysql.jdbc.Driver";
+    final static String url = "jdbc:mysql://fk.edu.pl";
+    final static String uid = "fkedupl_pwn";
+    final static String pwd = "pWn20180527";
+    private Connection con = null;
+
+    public DaoToMySQL() {
+        try {
+            Class.forName(draiverMySQl).newInstance();
+            con = DriverManager.getConnection(url,uid,pwd);
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            System.out.println("Nie znaleziono sterownika bazy danych");
+            System.out.println(e);
+            System.exit(1);
+        } catch (SQLException e) {
+            System.out.println("Nie połaczyłem się z bazą " + url);
+            System.out.println(e);
+            System.exit(1);
+        }
+    }
+
+    public Connection getCon() {
+        return con;
+    }
 }
+
