5f19d66240d1f4160f1fd3d0b2bace718e3c7082
Anzelm12 <wicio.holy@gmail.com>
2018-05-27 12:25:11 +0200

    klasy modelowe

--- src/javaFX_obslugaHarmonogramow/model/Kurs.java
+++ src/javaFX_obslugaHarmonogramow/model/Kurs.java
@@ -2,12 +2,22 @@ package javaFX_obslugaHarmonogramow.model;
 
 public class Kurs {
 
+    private int kID;
     private String kNazwa;
     private int kIleDni;
 
-    public Kurs(String kNazwa, int kIleDni) {
+    public Kurs(int kID, String kNazwa, int kIleDni) {
+        this.kID = kID;
         this.kNazwa = kNazwa;
         this.kIleDni = kIleDni;
+    }
+
+    public int getkID() {
+        return kID;
+    }
+
+    public void setkID(int kID) {
+        this.kID = kID;
     }
 
     public String getkNazwa() {
@@ -25,4 +35,5 @@ public class Kurs {
     public void setkIleDni(int kIleDni) {
         this.kIleDni = kIleDni;
     }
+
 }
